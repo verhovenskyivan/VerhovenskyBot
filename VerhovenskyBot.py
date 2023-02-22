0b709ca9a3ce54db40617d3e0bb54eef0faa330d
Verhovensky ivan <verhovenskyivan@gmail.com>
2023-02-22 22:20:53 +0300

    added bot command

--- VerhovenskyBot.py
+++ VerhovenskyBot.py
@@ -4,6 +4,7 @@ bot = telebot.TeleBot('6196881154:AAEyeS3nAfSQnrGlmAEaFytutdsdMbWH9yY')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Здравствуйте, я бот Ивана, принимаю сообщения и передаю их создателю', parse_mode='html')
+    mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+    bot.send_message(message.chat.id,mess, 'Здравствуйте, я бот Ивана, принимаю сообщения и передаю их создателю', parse_mode='html')
 
 bot.polling(non_stop=True)
